22f250cec5edb8b2d5aee59ca859af4de8cb1687
Vanessa Gonçalves <117444679+vangoncalves@users.noreply.github.com>
2024-02-23 16:50:24 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,71 @@
+const botaoPlayPause = document.getElementById('play-pause');
+const audioCapitulo = document.getElementById('audio-capitulo');
+const botaoAvancar = document.getElementById('proximo');
+const botaoVoltar = document.getElementById('anterior');
+const nomeCapitulo = document.getElementById('capitulo');
+const numeroCapitulos = 10;
+let taTocando = 0;
+let capituloAtual = 1;
+function tocarFaixa()
+{
+    audioCapitulo.play();
+    botaoPlayPause.classList.remove('bi-play-circle-fill');
+    botaoPlayPause.classList.add('bi-pause-circle-fill')
+}
+function pausarFaixa()
+{
+    audioCapitulo.pause();
+    botaoPlayPause.classList.add('bi-play-circle-fill');
+    botaoPlayPause.classList.remove('bi-pause-circle-fill')
+}
+
+function tocarOuPausar()
+{
+    if(taTocando === 0)
+    {
+        tocarFaixa();
+        taTocando = 1;
+    }
+    else
+    {
+        pausarFaixa();
+        taTocando = 0;
+    }
+}
+function proximaFaixa()
+{
+    if(capituloAtual === numeroCapitulos)
+    {
+        capituloAtual = 1;
+    }
+    else
+    {
+        capituloAtual = capituloAtual + 1;
+    }
+    audioCapitulo.src = './books/dom-casmurro/' + capituloAtual + '.mp3';
+    tocarFaixa();
+    taTocando = 1;
+    trocarNomeFaixa();
+}
+function voltarFaixa()
+{
+    if(capituloAtual === 1)
+    {
+        capituloAtual = numeroCapitulos;
+    }
+    else
+    {
+        capituloAtual = capituloAtual - 1;
+    }
+    audioCapitulo.src ="./books/dom-casmurro/" + capituloAtual + ".mp3";
+    tocarFaixa();
+    taTocando = 1;
+    trocarNomeFaixa();
+}
+function trocarNomeFaixa()
+{
+    nomeCapitulo.innerText = 'Capitulo' + capituloAtual;
+}
+botaoVoltar.addEventListener('click', voltarFaixa)
+botaoAvancar.addEventListener('click', proximaFaixa);
+botaoPlayPause.addEventListener('click', tocarOuPausar);
